4d1825dab32287b2f380a8b619942a4fe533aeff
arh <haghdoost@gmail.com>
2013-01-01 23:26:49 -0600

    initial stat collector, not completed yet

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,8 @@
 #include "stats.h"
 
+using namespace std; 
+
+
 void collectStat( uint32_t newFlags){
-	
+
 }
--- stats.h
+++ stats.h
@@ -1,6 +1,40 @@
 #ifndef STATS
 #define STATS
 #include <stdint.h>
+#include <string>
+
+
+class Stat {
+private:
+	uint64_t counter;
+	std::string name;
+public:
+	Stat(){
+		counter = 0;
+	}
+	Stat(uint64_t value){
+		counter = value; 
+	}
+	inline bool operator==(const Stat & other) const {
+		return (counter == other.counter); //this one is faster
+	}
+	inline bool operator++() {
+		return (++ counter); //this one is faster
+	}
+	std::string print() {
+		return name.append(",\t").append(std::string( std::to_string(counter) ) );
+	}
+};
 
 void collectStat( uint32_t newFlags);
+
+class StatsDS {
+public:
+	Stat l1PageHit;
+	Stat l1PageMiss;
+	Stat l1PageEvict;
+	Stat l1BlockHit;
+	Stat l1BlockMiss;
+	Stat l1BlockEvict;
+};
 #endif
